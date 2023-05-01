12154f971312350324b33dbb2d6eed78e8edede1
PierreWoL <118140290+PierreWoL@users.noreply.github.com>
2023-05-01 12:05:32 +0100

    subject all fixed

--- judge.py
+++ judge.py
@@ -1,12 +1,20 @@
 import os
 import pandas as pd
-path = os.getcwd()+"/datasets/open_data/Test"
-tables = [fn for fn in os.listdir(path) if '.csv' in fn]
-for table in tables:
-    fn = os.path.join(path, table)
-    try:
-        table = pd.read_csv(fn,lineterminator='\n')
-    except UnicodeDecodeError as e:
-        print("Oops!  should delete this file!",table)
-        os.remove(os.path.join(path, table))
+path_f = os.getcwd()+"/datasets/"
+datasets = ["SOTAB","T2DV2","Test_corpus"]
+folder = ['Test','SubjectColumn']
+for dataset in datasets:
+    path = os.path.join(path_f, dataset)
+    print(path)
+    for i in folder:
 
+
+        tables = [fn for fn in os.listdir(os.path.join(path, i)) if '.csv' in fn]
+        for table in tables:
+            fn = os.path.join(path, i, table)
+            try:
+                # print(table)
+                table = pd.read_csv(fn, lineterminator='\n')
+            except UnicodeDecodeError as e:
+                print("Oops!  should delete this file!", table)
+                os.remove(os.path.join(path, i, table))
