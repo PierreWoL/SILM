a3cd0fa14ce07be327bda16e51a0f96ffcb49d9f
PierreWoL <118140290+PierreWoL@users.noreply.github.com>
2023-05-31 23:39:39 +0100

    M3 COLUMN MATCHING COMPLETE

---
+++ ColumnAlignment/ColumnMatch.py
@@ -0,0 +1,85 @@
+import math
+import os
+
+import numpy as np
+import torch
+from ColumnMatchingClassifier import ColumnMatchingClassifier as Classifier
+from transformers import TrainingArguments
+from argparse import Namespace
+from Dataset_dict import create_column_pairs
+
+
+class ColumnMatch:
+
+    def __init__(self,
+                 train_path,
+                 hp: Namespace):
+        self.train_path = train_path
+        torch.cuda.empty_cache()
+        self.bert_trainer = Classifier.from_hp(train_path, hp)
+        epoch_steps = len(self.bert_trainer.train) // hp.batch  # total steps of an epoch
+        logging_steps = int(epoch_steps * 0.02) if int(epoch_steps * 0.02) > 0 else 5
+        eval_steps = 5 * logging_steps
+        training_args = TrainingArguments(
+            output_dir=hp.output_dir,
+            num_train_epochs=hp.num_epochs,
+            per_device_train_batch_size=hp.batch,
+            per_device_eval_batch_size=hp.batch,
+            weight_decay=0.01,
+            logging_steps=logging_steps,
+            logging_dir=f"{hp.output_dir}/tb",
+            eval_steps=eval_steps,
+            evaluation_strategy="steps",
+            do_train=True,
+            do_eval=True,
+            save_steps=eval_steps,
+            load_best_model_at_end=True,
+            save_total_limit=1,
+            metric_for_best_model="accuracy",
+            greater_is_better=True,
+        )
+
+        self.bert_trainer.train(train_args=training_args, do_fine_tune=hp.fine_tune)
+        if hp.fine_tune:
+            self.bert_trainer.trainer.save_model(
+                output_dir=os.path.join(hp.output_dir, "FT_columnMatch")
+            )
+            print("fine-tuning done, fine-tuned model saved.")
+        else:
+            print("pretrained or fine-tuned model loaded.")
+
+        self.bert_trainer.model.eval()
+        self.device = torch.device(f"cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.bert_trainer.model.to(self.device)
+
+        self.tokenize = lambda x: self.bert_trainer.tokenizer(
+            x, max_length=hp.max_length, truncation=True, padding=True, return_tensors="pt"
+        )
+        softmax = torch.nn.Softmax(dim=1)
+        self.classifier = lambda x: softmax(self.bert_trainer.model(**x).logits)[:, 1]
+
+    def score(self,  hp: Namespace):
+        r"""Score the samples with the classifier.
+
+            Args:
+                hp: parameters
+        """
+        samples, samples_mapping = create_column_pairs(hp.eval_path)
+        if len(samples)!=0 and len(samples_mapping)!=0:
+            sample_size = len(samples)
+            scores = np.zeros(sample_size)
+            batch_num = math.ceil(sample_size / hp.eval_batch_size)
+            for i in range(batch_num):
+                j = (i + 1) * hp.eval_batch_size \
+                    if (i + 1) * hp.eval_batch_size <= sample_size else sample_size
+                inputs = self.tokenize(samples[i * hp.eval_batch_size:j])
+                inputs.to(self.device)
+                with torch.no_grad():
+                    batch_scores = self.classifier(inputs)
+                scores[i * hp.eval_batch_size:j] = batch_scores.cpu().numpy()
+            results = {samples_mapping[i]: scores[i] for i in range(len(samples_mapping))}
+            print(results)
+            return results
+        else:
+            print("Wrong Path!")
+
---
+++ ColumnAlignment/ColumnMatchingClassifier.py
@@ -0,0 +1,137 @@
+# Copyright 2023 Jiaoyan Chen. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+import pickle
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# @paper(
+#     "Contextual Semantic Embeddings for Ontology Subsumption Prediction (World Wide Web Journal)",
+# )
+from datasets import Dataset
+from argparse import Namespace
+from Dataset_dict import dataframe_train, create_column_pairs_mapping
+import os
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    EarlyStoppingCallback,
+    Trainer,
+    TrainingArguments,
+)
+from sklearn.metrics import accuracy_score
+
+lm_mp = {'roberta': 'roberta-base',
+         'bert': 'bert-base-uncased',
+         'distilbert': 'distilbert-base-uncased',
+         'sbert': 'sentence-transformers/all-mpnet-base-v2'}
+
+
+def save_dict(dataset_dict, path):
+    with open(path, "wb") as f:
+        pickle.dump(dataset_dict, f)
+
+
+class ColumnMatchingClassifier:
+    def __init__(self,
+                 train_path,
+                 positive_op,
+                 max_len=256,
+                 lm='roberta',
+                 early_stop: bool = False,
+                 early_stop_patience: int = 10):
+        self.tokenizer = AutoTokenizer.from_pretrained(lm_mp[lm], selectable_pos=1)
+        self.model = AutoModelForSequenceClassification.from_pretrained(lm_mp[lm])
+        self.positive_op = positive_op
+        self.trainer = None
+        self.mapping = []
+        self.train_path = train_path
+        if any(file.endswith('.csv') for file in os.listdir(train_path)):
+            dfs = dataframe_train(self.train_path)
+            self.dataset = create_column_pairs_mapping(dfs, positive_op)[0]
+            self.train = self.load_dataset(self.dataset["train"], max_length=512)
+            self.eval = self.load_dataset(self.dataset["eval"], max_length=512)
+            """TODO this may need specified path"""
+            save_dict(self.dataset, os.getcwd()+"/dataset/dataset_dict.pkl")
+
+        else:
+            with open(os.getcwd()+"/dataset/dataset_dict.pkl", 'rb') as f:
+                self.dataset = pickle.load(f)
+
+        self.max_length = max_len
+        self.tables = [fn for fn in os.listdir(train_path) if '.csv' in fn]
+
+        # methods to create positive samples
+        print(f"data files loaded with sizes:")
+        print(f"\t[# Train]: {len(self.train)}")
+
+        # early stopping
+        self.early_stop = early_stop
+        self.early_stop_patience = early_stop_patience
+        self.table_cache = {}
+
+    @staticmethod
+    def from_hp(path: str, hp: Namespace):
+        """Construct Column matching from hyperparameters
+        Args:
+            path (str): the path to the table directory
+            hp (Namespace): the hyperparameters
+        Returns:
+            ColumnMatching: the constructed column pairs from different tables
+        """
+        return ColumnMatchingClassifier(path,
+                                        positive_op=hp.positive_op,
+                                        lm=hp.lm,
+                                        max_len=hp.max_len,
+                                        early_stop=hp.early_stop,
+                                        early_stop_patience=hp.early_stop_patience
+                                        )
+
+    "slice or random select the cells from the particular columns"
+
+    def train(self, train_args: TrainingArguments, do_fine_tune: bool = True):
+        r"""Initiate the Huggingface trainer with input arguments and start training.
+        Args:
+            train_args (TrainingArguments): Arguments for training.
+            do_fine_tune (bool): `False` means loading the checkpoint without training. Defaults to `True`.
+        """
+        self.trainer = Trainer(
+            model=self.model,
+            args=train_args,
+            train_dataset=self.train,
+            eval_dataset=self.eval,
+            compute_metrics=self.compute_metrics,
+            tokenizer=self.tokenizer,
+        )
+        if self.early_stop:
+            self.trainer.add_callback(EarlyStoppingCallback(early_stopping_patience=self.early_stop_patience))
+        if do_fine_tune:
+            self.trainer.train()
+
+    @staticmethod
+    def compute_metrics(pred):
+        """Auxiliary function to add accurate metric into evaluation.
+        """
+        labels = pred.label_ids
+        preds = pred.predictions.argmax(-1)
+        acc = accuracy_score(labels, preds)
+        return {"accuracy": acc}
+
+    def load_dataset(self, dataset: Dataset, max_length: int = 512) -> Dataset:
+        r""" Preprocess the input data
+        Args:
+            dataset (Dataset): the training Dataset generated previously from the pairs of columns
+            max_length (int): Maximum length of the input sequence.
+        """
+
+        def preprocess_function(examples):
+            return self.tokenizer(examples["Col1"], examples["Col2"], max_length=max_length, truncation=True)
+
+        dataset = dataset.map(preprocess_function, batched=True, num_proc=1, )
+        return dataset
