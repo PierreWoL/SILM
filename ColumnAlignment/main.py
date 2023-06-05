73721939747e2771d095a6a5de4abdc24609a553
PierreWoL <118140290+PierreWoL@users.noreply.github.com>
2023-06-05 22:47:06 +0100

    M1 2 --Header

--- ColumnAlignment/main.py
+++ ColumnAlignment/main.py
@@ -6,16 +6,16 @@ from metrics import Ground_truth
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--method", type=str, default="M1")  # M1 M2 M3
+    parser.add_argument("--method", type=str, default="M1H")  # M1 M2 M3
     parser.add_argument("--dataset", type=str, default="Wikidata")  # ChEMBL Magellan OpenData TPC-DI Wikidata
-    parser.add_argument("--ground_truth_path", type=str, default="data/data_mapping.json")  #
-    parser.add_argument("--eval_path", type=str, default="data/")  # data/
+    parser.add_argument("--ground_truth_path", type=str, default="D:/CurrentDataset/ValentineDatasets/Wikidata/musicians_joinable/musicians_joinable_mapping.json")  #data/data_mapping.json
+    parser.add_argument("--eval_path", type=str, default="D:/CurrentDataset/ValentineDatasets/Wikidata/musicians_joinable")  # data/
     parser.add_argument("--fine_tune", dest="fine_tune", action="store_true", default=False)
     parser.add_argument("--logdir", type=str, default="model/")
     parser.add_argument("--output_dir", type=str, default="model/")
     parser.add_argument("--num_epochs", type=int, default=10)
     parser.add_argument("--batch", type=int, default=16)
-    parser.add_argument("--eval_batch_size", type=int, default=16)
+    parser.add_argument("--eval_batch_size", type=int, default=2)
     parser.add_argument("--max_len", type=int, default=256)
     parser.add_argument("--lr", type=float, default=5e-5)
     parser.add_argument("--early_stop", type=bool, default=False)
@@ -32,6 +32,10 @@ if __name__ == '__main__':
     if hp.method == "M1" or hp.method == "M2":
         match = SimpleColumnMatch(hp.eval_path, hp.method)
         score = match.SimpleMatch(0.55)
+        Ground_truth(hp.eval_path,hp.ground_truth_path, score, hp.eval_path + "/results/", hp.method)
+    if hp.method == "M1H" or hp.method == "M2H":
+        match = SimpleColumnMatch(hp.eval_path, hp.method)
+        score = match.SimpleMatch_Header(0.55)
         Ground_truth(hp.eval_path,hp.ground_truth_path, score, hp.eval_path + "/results/", hp.method)
     if hp.method == "M3":
         current_dir = os.getcwd()
