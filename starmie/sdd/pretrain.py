a3b1e4e313f345db7ab1c3a3851289c5bf77505e
PierreWoL <118140290+PierreWoL@users.noreply.github.com>
2023-09-27 21:34:38 +0100

    all_finish

--- starmie/sdd/pretrain.py
+++ starmie/sdd/pretrain.py
@@ -102,25 +102,26 @@ def train(trainset, hp):
 
             # save the checkpoints for each component
             ckpt_path = os.path.join(hp.logdir, hp.method, hp.dataset, 'model_' +
-                                     str(hp.augment_op) + "lm_" + str(
+                                     str(hp.augment_op) + "_lm_" + str(
                 hp.lm) + "_" + str(hp.sample_meth) + "_" + str(
                 hp.table_order) + '_' + str(
                 hp.run_id) + "_" + str(hp.check_subject_Column) + ".pt")
             if hp.single_column:
                 ckpt_path = os.path.join(hp.logdir, hp.method, hp.dataset, 'model_'+
-                                         str(hp.augment_op) +"lm_"+ str(
+                                         str(hp.augment_op) +"_lm_"+ str(
                                              hp.lm)+ "_" + str(hp.sample_meth) + "_" + str(
                                              hp.table_order) + '_' + str(
                                              hp.run_id) + "_" + str(hp.check_subject_Column)+ "_singleCol.pt")
-            if hp.subject_column:
+            elif hp.subject_column:
                 ckpt_path = os.path.join(hp.logdir, hp.method, hp.dataset, 'model_'+
-                                         str(hp.augment_op) +"lm_"+ str(
+                                         str(hp.augment_op) +"_lm_"+ str(
                                              hp.lm)+ "_" + str(hp.sample_meth) + "_" + str(
                                              hp.table_order) + '_' + str(
                                              hp.run_id) + "_" + str(hp.check_subject_Column)+ "_subCol.pt")
-            if hp.header:
+                print(ckpt_path)
+            elif hp.header:
                 ckpt_path = os.path.join(hp.logdir, hp.method, hp.dataset, 'model_'+
-                                         str(hp.augment_op) +"lm_"+ str(
+                                         str(hp.augment_op) +"_lm_"+ str(
                                              hp.lm)+ "_" + str(hp.sample_meth) + "_" + str(
                                              hp.table_order) + '_' + str(
                                              hp.run_id) + "_" + str(hp.check_subject_Column)+ "_header.pt")
