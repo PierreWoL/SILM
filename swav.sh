19d1cfd2b6939ece9f442587763bd1c396b35cad
PierreWoL <zhenyu.wu@postgrad.manchester.ac.uk>
2024-07-28 20:04:11 +0100

    change script

--- swav.sh
+++
@@ -1,1 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=1  E:/Project/CurrentDataset/learning/SwAV.py
--- train.sh
+++ train.sh
@@ -1,18 +1,21 @@
 #!/bin/bash --login
 #$ -cwd 
-#$ -l a100=1         # A 1-GPU request (v100 is just a shorter name for nvidia_v100)
+#$ -l a100=2         # A 1-GPU request (v100 is just a shorter name for nvidia_v100)
                      # Can instead use 'a100' for the A100 GPUs (if permitted!)
                      # 8 CPU cores available to the host code
                      # Can use up to 12 CPUs with an A100 GPU.
 module load libs/cuda
 conda activate py39
 source /mnt/iusers01/fatpou01/compsci01/c29770zw/test/CurrentDataset/datavenv/bin/activate
-datasets=("WDC" "GDS" "GoogleSearch")
-check_subject_Columns=("none" "header")
-models=("sbert" "bert" "roberta")
-augmentation=("sample_cells" "sample_cells_TFIDF") #"sample_cells"  sample_cells_TFIDF
-aug_times=(7) #1
+datasets=("WebData")
+check_subject_Columns=("none")
+models=("sbert")
+augmentation=("sample_cells_TFIDF") #"sample_cells"  sample_cells_TFIDF  "header"
+aug_times=(2) #1
+selected_datasets=(200 400 800 1600)
 for dataset in "${datasets[@]}"
+do
+for num in "${selected_datasets[@]}"
 do
   for check_subject_Column in "${check_subject_Columns[@]}"
   do
@@ -30,26 +33,22 @@ do
                     else
                         augment_methods+="$aug,"
                     fi
-                    if (( time==7 )); then
-                      batch=7
-                      epoches=10
-                    elif (( time==8 )); then
-                      batch=1
-                      epoches=10
+                    if (( time>=6 )); then
+                      batch=8
+                      epoches=12
+                    else
+                        batch=64
+                        epoches=35
                     fi
                   done
-                  echo $dataset $check_subject_Column $model "$time $augment_methods batch $batch poches $epoches" 
+                  echo $dataset $check_subject_Column $model "$time $num $augment_methods batch $batch epoch $epoches" 
                   python pretrain_all.py \
-                  --method starmie \
                   --dataset $dataset \
                   --batch_size $batch \
-                  --lr 5e-5 \
+                  --datasetSize $num \
                   --lm $model \
                   --fp16 \
                   --n_epochs $epoches \
-                  --max_len 256 \
-                  --size 10000 \
-                  --projector 768 \
                   --save_model \
                   --column \
                   --augment_op $augment_methods \
@@ -60,5 +59,6 @@ do
           done
         done
       done
+  done
   done
 done
