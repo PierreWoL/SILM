b8583f1ec7ade352edf279cb2731d11414866340
kupo <118140290+PierreWoL@users.noreply.github.com>
2026-05-12 18:38:24 +0100

    adding supporting materials

---
+++ EmSI/P1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+#$ -cwd
+#$ -l mem512     # For 32GB per core, any of the CPU types below (system chooses)
+
+module load libs/cuda
+conda activate py39
+source /mnt/iusers01/fatpou01/compsci01/c29770zw/test/CurrentDataset/datavenv/bin/activate
+datasets=("WDC" "GDS")
+models=("sbert")
+for data in "${datasets[@]}"
+do
+for model in "${models[@]}"
+do
+      for delta in "${deltas[@]}"
+      do
+      echo $model   $delta
+      python main.py \
+      --embed $model \
+      --dataset $data \
+      --step 1
+    done
+done
+done
+
